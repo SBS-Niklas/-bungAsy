1ef590e3c769148743c3b6009d0043581e40abab
Niklas <zeilerniklas11@gmail.com>
2022-11-28 17:20:19 +0100

    Projektdateien hinzufügen.

---
+++ ÜbungAsy/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ÜbungAsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AsyncMethodAuslesen().Result);
+            AsyncMethodEingabe();
+            AsyncMethodEingabe();
+            Console.WriteLine(AsyncMethodAuslesen().Result);
+
+            Console.ReadKey();
+
+        }
+
+        static async Task<string> AsyncMethodAuslesen()
+        {
+            return await File.ReadAllTextAsync("File1.txt");
+        }
+
+        static  void AsyncMethodEingabe()
+        {
+           
+             File.AppendAllTextAsync("File1.txt", "Ich wurde eingefügt im Programm!!");
+        }
+    }
+
+        
+}
